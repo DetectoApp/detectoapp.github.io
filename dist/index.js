9c5e8b14a0b2dbb5bce188333df3a0b72d3dbe8c
Gianluigi Memoli <gianluigi.memoli@fem.digital>
2024-11-19 10:34:26 +0100

    removed transition on feature accordion

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+
+document.querySelectorAll('[x-component]').forEach(component => {
+    const componentName = `x-${component.getAttribute('x-component')}`
+    class Component extends HTMLElement {
+        connectedCallback() {
+            this.append(component.content.cloneNode(true))
+        }
+
+        data() {
+            const attributes = this.getAttributeNames()
+            const data = {}
+            attributes.forEach(attribute => {
+                data[attribute] = this.getAttribute(attribute)
+            })
+            return data
+        }
+    }
+    customElements.define(componentName, Component)
+})
